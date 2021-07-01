118cda02b133d951741f7bade14c489a143d226e
Danilo Silva <danilo.dos.s.silva@gmail.com>
2021-07-01 15:59:15 -0300

    Inseri os atributos id, foto e tipo no model UsarioLogin

--- BACK-END/Desenvolvimento/lotus/src/main/java/br/com/generation/los4hermanos/lotus/model/UsuarioLogin.java
+++ BACK-END/Desenvolvimento/lotus/src/main/java/br/com/generation/los4hermanos/lotus/model/UsuarioLogin.java
@@ -2,13 +2,26 @@ package br.com.generation.los4hermanos.lotus.model;
 
 public class UsuarioLogin {
 
+    private long id;
     private String nome;
     private String usuario;
     private String email;
     private String cpf;
     private String senha;
     private String token;
+    private String foto;
+    private String tipo;
     
+
+
+// ----------   GETTERS AND SETTERS ---------
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return this.nome;
@@ -56,6 +69,22 @@ public class UsuarioLogin {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public String getFoto() {
+        return this.foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
 
